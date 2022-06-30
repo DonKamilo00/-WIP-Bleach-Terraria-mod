68ee81535a9dda3ff852a59e697bf289bf103cb5
DonKamilo00 <maruszakkamil1@gmail.com>
2022-06-30 20:00:14 +0100

    Initial commit

---
+++ Bleachtest.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Bleachtest
+{
+	public class Bleachtest : Mod
+	{
+	}
+}
---
+++ Items/Test.cs
@@ -0,0 +1,39 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Bleachtest.Items
+{
+	public class Test : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("Test"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Zangetsu");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 500;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 60;
+			Item.height = 60;
+			Item.useTime = 30;
+			Item.useAnimation = 60;
+			Item.useStyle = 1;
+			Item.knockBack = 6;
+			Item.value = 10000;
+			Item.rare = 2;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
